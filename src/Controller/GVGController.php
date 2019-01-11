2832ed4410a12fe762c87f01fdff9a0d2876211c
Igor Goroun <igor.goroun@gmail.com>
2019-01-11 17:31:16 +0200

    Fix party sorting

--- src/Controller/GVGController.php
+++ src/Controller/GVGController.php
@@ -102,6 +102,8 @@ class GVGController extends AbstractController
             200
         );
 
+        dump($pagination);
+
         $parties = $em->getRepository('App:GVGParty')->findBy([], ['name'=>'ASC']);
         return $this->render('gvg/list-presence.html.twig', [
             'gvg' => $gvg,
